20dd4956248d8df1fd8256a140229d1ec4a3c8bc
Robert Huguet <Robert_Huguet@Hotmail.com>
2019-02-22 00:53:43 -0700

    tests
    tests in homework 4 examples

---
+++ Homework-4/Course_Example_Tests/sketch.js
@@ -0,0 +1,86 @@
+function setup() {
+    createCanvas(800, 500);
+    background('#e3f9c8');
+}
+
+function draw() {
+
+    //ellipse test
+    //ellipse( 10, 10, 16, 16 );
+    //point test
+    //strokeWeight(10);
+    //point( 100, 50 );
+    //multi point test
+    //point(30, 20);
+    //point(85, 20);
+    //point(85, 75);
+    //point(30, 75);
+    //rect test
+    //rect( 25, 30, 450, 540);
+    //two rectangle test
+    //rect(20, 20, 300, 200, 10);
+    //rect(20, 280, 300, 200, 90);
+    // a rectangle with various corner roundness
+    //rect( 20, 20, 300, 300, 0, 90, 45, 150 );
+    //triangle example
+    //triangle( 50, 50, 250, 50, 150, 180 );
+    //triangle( 350, 50, 550, 50, 450, 180 );
+    //triangle( 10, 225, 590, 225, 375, 375 );
+    //line examples
+    //line(30, 20, 85, 20);
+    //stroke(126);
+    //line(85, 20, 85, 75);
+    //stroke(255);
+    //line(85, 75, 30, 75);
+    //quad example
+    //quad(38, 31, 86, 20, 69, 63, 30, 76)
+    //arc examples
+    //arc(50, 55, 50, 50, 0, HALF_PI);
+    //noFill();
+    //arc(50, 55, 60, 60, HALF_PI, PI);
+    //arc(50, 55, 70, 70, PI, PI + QUARTER_PI);
+    //arc(50, 55, 80, 80, PI + QUARTER_PI, TWO_PI);arc(50, 55, 50, 50, 0, HALF_PI);
+    //noFill();
+    //arc(50, 55, 60, 60, HALF_PI, PI);
+    //arc(50, 55, 70, 70, PI, PI + QUARTER_PI);
+    //arc(50, 55, 80, 80, PI + QUARTER_PI, TWO_PI);
+    //arc( 300, 200, 300, 300, 0, HALF_PI );
+    //arc( 300, 200, 300, 300, QUARTER_PI, HALF_PI );
+    //arc( 300, 200, 300, 300, radians(260), radians(280) );
+    //arc( 150, 200, 180, 180, HALF_PI, 0, PIE );
+    //arc( 450, 200, 180, 180, HALF_PI, 0, CHORD );
+    //fill examples
+    //fill('blue');
+    //rect( 20, 20, 480, 50 );
+    //ellipse( 250, 105, 400, 50 );
+
+    //fill('yellow');
+    //ellipse( 250, 160, 300, 50 );
+
+    //fill('orange');
+    //triangle( 20, 205, 250, 255, 480, 255 );
+    //rect( 60, 275, 120, 50 );
+    //fill('purple');
+    //rect( 20, 20, 300, 100 );
+
+    //noFill();
+    //rect( 150, 70, 300, 150 );
+
+    //fill('yellow');
+    //ellipse( 250, 270, 300, 150 );
+
+    //noFill();
+    //triangle( 20, 330, 250, 20, 480, 330 );
+    //stroke examples
+    noFill();
+
+    stroke('red');
+    rect( 20, 20, 300, 100 );
+
+    stroke('green');
+    triangle( 20, 330, 250, 20, 480, 330 );
+
+    stroke('pink');
+    fill('yellow');
+    ellipse( 250, 250, 280, 130 );
+}
