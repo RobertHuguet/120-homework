9aef4c433d56ba75abd519fac6354c9de4037af6
Robert Huguet <Robert_Huguet@Hotmail.com>
2019-02-28 22:48:23 -0700

    added homework 5 and tests file
    files to work on

---
+++ Homework-5/Class_Examples_Tests/sketch.js
@@ -0,0 +1,24 @@
+//function setup() {
+//    createCanvas( 800, 600 );
+//    background( 'rgb(255, 210, 94)' );
+//}
+
+//function draw() {
+//    noCursor();
+//    fill( 255 );
+//    ellipse( mouseX, mouseY, 40 );
+//}
+//function setup() {
+    //createCanvas( windowWidth, windowHeight );
+    //createCanvas( windowWidth, 600 );
+//}
+
+
+//function draw() {
+    //background( 'rgb(0, 6, 87)' );
+    //textSize(36);
+    //noStroke();
+    //fill('white');
+    //text("'mouseX': " + floor(mouseX), 10, 40 );
+    //text("'mouseY': " + floor(mouseY), 10, 80 );
+//}
