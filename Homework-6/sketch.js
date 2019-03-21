d461aa4b914c50e825387b88ccdbf044a86f3756
Robert Huguet <Robert_Huguet@Hotmail.com>
2019-03-20 23:10:03 -0600

    finished sketch HW6
    finished sketch HW6

--- Homework-6/sketch.js
+++ Homework-6/sketch.js
@@ -1,14 +1,15 @@
-//Initialize the locations of 2 circles at a global level
+//Initialize the locations of 2 ellipse at a global level
 let circle = {
-  x: 50,
-  y: 100,
+  x: 0,
+  y: 0,
   s: 0
 };
 
 let circle1 = {
-  x: 100,
-  y: 50,
-  s: 0
+  x: 0,
+  y: 0,
+  w: 0,
+  h: 0
 };
 
 //Global color for background changes
@@ -42,20 +43,39 @@ frameRate(7);
 function mouseClicked() {
   colorBg.r = map(mouseX, 0, windowWidth, 0, 255);
   colorBg.g = map(mouseY, 0, windowHeight, 0, 255);
-  colorBg.b = floor(random(256));
+  colorBg.b = floor(random(0, 255));
 
   background(colorBg.r, colorBg.g, colorBg.b);
 };
 
 function draw() {
+  fill(255);
+  stroke(1);
+  rect(0, 15, 320, 40);
+  fill(0);
+  noStroke();
+  text("Click the mouse in different places!", 10, 30);
+  text("The colors change based on where you click the mouse.", 10, 45);
 
 // Setup and draw circle
+  noStroke();
   colorSp.r = map(mouseX, 0, windowWidth, 0, 255);
   colorSp.g = map(mouseY, 0, windowWidth, 0, 255);
-  colorSp.b = floor(random(256));
-  circle.x = ceil(random(windowWidth - 20));
-  circle.y = ceil(random(windowHeight - 20));
-  circle.s = floor(random(21));
-  fill(colorSp.r, colorSp.g, colorSp.b)
+  colorSp.b = floor(random(0, 255));
+  colorSp.b = colorSp.b + (colorSp.b % colorSp.r);
+  circle.x = ceil(random(0, windowWidth - 20));
+  circle.y = ceil(random(0, windowHeight - 20));
+  circle.s = floor(random(0, 20));
+  fill(colorSp.r, colorSp.g, colorSp.b);
   ellipse(circle.x, circle.y, circle.s);
+
+  colorSp1.r = colorSp.r - random(0, 20);
+  colorSp1.g = colorSp.g - random(0, 20);
+  colorSp1.b = colorSp.b;
+  circle1.x = ceil(random(0, windowWidth - 20));
+  circle1.y = ceil(random(0, windowHeight - 20));
+  circle1.w = floor(random(0, 30));
+  circle1.h = floor(random(0, 60));
+  fill(colorSp1.r, colorSp1.g, colorSp1.b, random(50, 150));
+  ellipse(circle1.x, circle1.y, circle1.w, circle1.h);
 };
